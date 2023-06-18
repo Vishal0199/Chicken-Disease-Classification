8c6860204d362df36272919da7a6ca7e7548dbdd
Vishalkumar Singh <vishal.a.singh01@gmail.com>
2023-06-18 17:31:24 +0530

    Prediction

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -100,7 +100,9 @@ class ConfigurationManager:
 
         return training_config
     
-    
+
+
+
     def get_validation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model=Path("artifacts/training/model.h5"),
@@ -109,5 +111,6 @@ class ConfigurationManager:
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
         )
-        return eval_config    
-    
+        return eval_config
+
+      
